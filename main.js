56fc120c9dabd00e0be21f4496e95c9821617dcd
Andrés García <andresgarcia0313@gmail.com>
2024-03-11 09:18:38 -0500

    Capturar datos en consola y imprimirlas

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log("Bienvenidos");
+const readline = require("readline").createInterface({
+  //importamos la libreria readline para poder leer la entrada del usuario
+  input: process.stdin, //entrada
+  output: process.stdout, //salida
+});
+/**
+ * @name input
+ * Función que se encarga de pedir un texto con un mensaje al usuario
+ * @param {String} message
+ * @returns
+ */
+const input = (message) => {
+  return new Promise((resolve) => {
+    readline.question(message, resolve);
+  });
+};
+/**
+ * Función principal que se encarga de pedir datos y mostrar un mensaje
+ */
+const main = async () => {
+  let data = await input("Por favor, ingresa el número: 😊 ");
+  //realizar la conversión a numero
+  data=parseInt(data);
+  console.log(`¡Hola! Gracias por ingresar esto: 🎉: ${data}`);
+};
+
+main(); //Ejecutamos la función principal
